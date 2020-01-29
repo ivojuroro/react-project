3f50b5ff7004e09dc86b9970f34b4ac1569a2e8c
ivojuroro <ivojuroro@gmail.com>
2020-01-29 10:24:32 +0100

    - Adding bootstrap styles - Adding styles folder

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Col, Row} from "react-bootstrap";
+
+let Home = (props) =>
+    <Row>
+        <Col md={1}>
+        </Col>
+        <Col md={3}>
+
+        </Col>
+        <Col md={7}>
+
+        </Col>
+        <Col md={1}>
+        </Col>
+    </Row>;
+
+export default Home;
