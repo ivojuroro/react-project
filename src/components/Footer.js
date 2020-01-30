1cb74d7e5899ceaefce768a5d81e174cb7708a69
juliana <j.sierrac@uniandes.edu.co>
2020-01-30 21:36:04 +0100

    small bug fix

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import '../styles/Footer.css'
+let Footer=(props)=>
+    <footer className="footer">
+        <p>Some footer nonsense!</p>
+    </footer>
+
+export default Footer;
